6a4e1d4bb8851208f2304c0c1d9b943f5164fa63
Dmitry Shilin <dmitryshilin@agileorange.ru>
2012-09-07 14:57:01 +0400

    disable ssl in production

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,4 +9,5 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
+  Spree::Config.set(:allow_ssl_in_production => false)
 end
